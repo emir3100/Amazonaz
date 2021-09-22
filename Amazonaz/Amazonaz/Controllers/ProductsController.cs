f086add130491ec6a4cd53a824ea7c90ac52cd84
Emir Jasarevic <61988938+emir3100@users.noreply.github.com>
2021-09-23 00:34:00 +0200

    Products Controller

--- Amazonaz/Amazonaz/Controllers/ProductsController.cs
+++ Amazonaz/Amazonaz/Controllers/ProductsController.cs
@@ -1,6 +1,9 @@
 ﻿using Amazonaz.Server.Data;
+using Amazonaz.Server.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Amazonaz.Server.Controllers;
 [Route("api/[controller]")]
@@ -13,12 +16,39 @@ public class ProductsController : ControllerBase
     {
         this._applicationDbConext = applicationDbConext;
     }
+
+    [HttpGet]
+    public async Task<IEnumerable<ProductModel>> GetAllProducts()
+    {
+        return await _applicationDbConext.Products.ToArrayAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetProductById(Guid id)
+    {
+        var product = await _applicationDbConext.Products.FindAsync(id);
+
+        if (product is null)
+            return NotFound();
+
+        return Ok(product);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateProduct(ProductModel product)
+    {
+        var user = await _applicationDbConext.Users.FindAsync(product.CreatedById);
+
+        if (user is null)
+            return BadRequest();
+        
+        product.Id = Guid.NewGuid();
+        product.CreationDate = DateTime.UtcNow;
+        _applicationDbConext.Add(product);
+        await _applicationDbConext.SaveChangesAsync();
+
+        return Created($"api/products/{product.Id}", product);
+    }
 }
 
 
-//Products endpoints:
-
-//Get All Products
-//Get Product with id
-//Create Product
-//Buy Product
